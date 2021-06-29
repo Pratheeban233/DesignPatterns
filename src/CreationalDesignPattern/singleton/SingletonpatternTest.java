dc4cc81709f4480b1ece7c283fb4b0081803ce1b
Pratheeban <pratheeban.eproc@gmail.com>
2021-06-29 11:14:35 +0530

    Singleton pattern using enum only after java1.5

--- src/CreationalDesignPattern/singleton/SingletonpatternTest.java
+++ src/CreationalDesignPattern/singleton/SingletonpatternTest.java
@@ -3,16 +3,34 @@ package CreationalDesignPattern.singleton;
 public class SingletonpatternTest {
 
 	public static void main(String[] args) {
+//
+//		Thread t1 = new Thread(() -> {
+//			Singleton singleton = Singleton.getInstance();
+//		});
+//
+//		Thread t2 = new Thread(() -> {
+//			Singleton singleton = Singleton.getInstance();
+//		});
+//		t1.start();
+//		t2.start();
 
-		Thread t1 = new Thread(() -> {
-			Singleton singleton = Singleton.getInstance();
-		});
+		ABC abc = ABC.INSTANCE;
+		abc.i = 5;
+		abc.show();
 
-		Thread t2 = new Thread(() -> {
-			Singleton singleton = Singleton.getInstance();
-		});
-		t1.start();
-		t2.start();
+		ABC abc1 = ABC.INSTANCE;
+		abc1.i = 6;
+
+		abc.show();
+
 	}
 
+}
+
+enum ABC{
+	INSTANCE;
+	int i ;
+	public void show(){
+		System.out.println(i);
+	}
 }
